1015e7fc5c5d7b97e8a41b19f269f5c8d15b7593
97Lucaas <lucas.duverneuil@mmibordeaux.com>
2023-03-16 16:47:51 +0100

    bidon2

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -56,7 +56,7 @@
                 document.getElementById("tbodylb").innerHTML += "<tr> <td>"+o.name+"</td> <td>"+o.score+"</td> </tr>";
             });
             }
-//bulkaijdijzd
+//bulkaijdijzd bidon
 
             /*
             fetch('/Increment',{
